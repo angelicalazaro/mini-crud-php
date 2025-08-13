ddf18a14fe755b240bb0697cf94dbec8bfc312da
Angelica <angelicamld@gmail.com>
2025-08-13 12:09:00 +0200

    adding post method and connecting to db with include_once function. Also insert sql request after declaring sql variable

--- mini-crud/User/addUser.php
+++ mini-crud/User/addUser.php
@@ -1,3 +1,27 @@
+<?php
+// ligne 'submit' / son name est 'send' se relie a la methode post
+    if(isset($_POST['send'])) {
+        if(isset($_POST['username']) &&
+           isset($_POST['email']) &&
+           $_POST['username'] != "" &&
+           $_POST['email'] != "" 
+
+        ){
+          include_once __DIR__ . "/../connect_db.php";
+          extract($_POST);
+
+          $sql = "INSERT INTO users (username, email) VALUES ('$username', '$email')";
+          if (mysqli_query($connexion, $sql)) {
+            header("location:showUser.php");
+          } else {
+            header("location:addUser.php?message=AddFail");
+          }
+        } else {
+            header("location:addUser.php?message=EmptyFields");
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,8 +33,8 @@
 <body>
     <form action="" method="post">
         <h1>Ajouter un utilisateur</h1>
-          <input type="text" name="Username" placeholder="utilisateur">
-          <input type="email" name="Email" placeholder="email">
+          <input type="text" name="username" placeholder="utilisateur">
+          <input type="email" name="email" placeholder="email">
           <input type="submit" value="Ajouter" name="send">
           <a class="link back" href="showUser.php">Annuler</a>
     </form>
