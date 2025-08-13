2ae8178fbf7518622436f3b685cf79182b3156e7
Angelica <angelicamld@gmail.com>
2025-08-13 14:49:32 +0200

    adding correct indent to modifyUser.php in the conditions

--- mini-crud/User/modifyUser.php
+++ mini-crud/User/modifyUser.php
@@ -1,3 +1,26 @@
+<?php
+    // declaration de variable user_id qui 
+    $user_id = $_GET['id'];
+        if(isset($_POST['send'])) {
+            if(isset($_POST['username']) &&
+                isset($_POST['email']) &&
+                $_POST['username'] != "" &&
+                $_POST['email'] != "" 
+            ){
+                include_once __DIR__ . "/../connect_db.php";
+                extract($_POST);
+
+                $sql = "UPDATE users SET username = '$username' , email = '$email' WHERE user_id = $user_id";
+                if (mysqli_query($connexion, $sql)) {
+                        header("location:showUser.php");
+                    } else {
+                        header("location:showUser.php?message=AddFail");
+                    }
+                } else {
+                    header("location:showUser.php?message=EmptyFields");
+            }
+        }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,12 +30,23 @@
     <link rel="stylesheet" href="../style.css">
 </head>
 <body>
+    <?php 
+        include_once __DIR__ . "/../connect_db.php";
+        // list of user information
+        $sql = "SELECT * FROM users where user_id = $user_id";
+        $result = mysqli_query($connexion, $sql);
+            // output data of each row
+            while ($row = mysqli_fetch_assoc($result)) {
+                 ?>
     <form action="" method="post">
         <h1>Modifier un utilisateur</h1>
-          <input type="text" name="Username" value="" placeholder="utilisateur">
-          <input type="email" name="Email" value="" placeholder="email">
+          <input type="text" name="Username" value="<?=$row['username']?>" placeholder="utilisateur">
+          <input type="email" name="Email" value="<?=$row['email']?>" placeholder="email">
           <input type="submit" value="Modifier" name="send">
           <a class="link back" href="showUser.php">Annuler</a>
     </form>
+    <?php
+            }
+    ?>
 </body>
 </html>
